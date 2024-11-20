08c8912fe21b6f0b3ab23bc80758e60aa3342648
E <raju.a.e@capgemini.com>
2024-11-20 09:16:53 +0530

    Playing with multiple route handlers and next function

--- src/app.js
+++ src/app.js
@@ -2,23 +2,31 @@ const express = require("express");
 
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Raju", lastName: "E" });
-});
-
-app.post("/user", (req, res) => {
-  // saving data to DB
-  res.send("Data saved in the database successfully");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("User deleted successfully");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello !!!");
-});
+app.use("/user", [
+  (req, res, next) => {
+    console.log("user response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("User response 2");
+    //res.send("successfully sent response 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("User response 3");
+    //res.send("successfully sent response 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("User response 4");
+    //res.send("successfully sent response 4");
+    next();
+  },
+  (req, res) => {
+    console.log("User response 5");
+    res.send("successfully sent response 5");
+  },
+]);
 
 app.listen(3000, () => {
   console.log("Server is listening to the port 3000 successfully");
